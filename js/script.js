cf7d415bc87e849835fe3aa3778e9baa73623c1d
BrianBontempi <brian.bont54@gmail.com>
2024-01-17 19:17:24 +0100

    add teams array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ! Primo snack
+
 // Creare un array di oggetti che descrivono biciclette da corsa
 const bicycles = [
     { name: 'cannondale', peso: 8.2 },
@@ -15,3 +17,20 @@ const bicycles = [
   }
   
   console.log(`La bici con il peso minore è ${lighterBike.name} con un peso di ${lighterBike.peso} kg.`);
+
+
+// ! Secondo snack
+
+// Funzione per generare un numero casuale compreso tra min e max
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Array di oggetti rappresentanti squadre di calcio
+  const teams = [
+    { nome: 'Juve', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Milan', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Inter', puntiFatti: 0, falliSubiti: 0 },
+  ];
+  
+  
