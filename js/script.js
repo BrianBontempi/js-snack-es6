b7d124c037cb2f5179f944470fb2904d19b41ccf
BrianBontempi <brian.bont54@gmail.com>
2024-01-17 19:13:38 +0100

    find the lighter bike

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const bicycles = [
     { name: 'bianchi', peso: 7.5 },
     { name: 'graziella', peso: 7.8 },
   ];
+
+  // Trovare la bici con il peso minore utilizzando destructuring
+  let lighterBike = bicycles[0];
+
+  for (const bike of bicycles) {
+    if (bike.peso < lighterBike.peso) {
+        lighterBike = bike;
+    }
+  }
+  
+  console.log(`La bici con il peso minore è ${lighterBike.name} con un peso di ${lighterBike.peso} kg.`);
