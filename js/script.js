64aeba76d46f3a0e318c2ee4a1a3398c88acd37a
BrianBontempi <brian.bont54@gmail.com>
2024-01-17 19:04:13 +0100

    add bike array

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Creare un array di oggetti che descrivono biciclette da corsa
+const bicycles = [
+    { name: 'cannondale', peso: 8.2 },
+    { name: 'bianchi', peso: 7.5 },
+    { name: 'graziella', peso: 7.8 },
+  ];
