2b4074aafcd147818df5e0c1cd53321cb80472f4
BrianBontempi <brian.bont54@gmail.com>
2024-01-22 16:33:43 +0100

    add guest array

---
+++ snack 1:2/js/script.js
@@ -0,0 +1,14 @@
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
